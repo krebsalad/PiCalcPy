2092053e0ccb4a28fdcfcf679058b35685cb4695
krebsalad <johanmgina7@gmail.com>
2019-10-09 22:19:57 +0200

    Update install_picalc_server.py

--- install_picalc_server.py
+++ install_picalc_server.py
@@ -7,4 +7,4 @@ subprocess.call(["wget", "https://raw.githubusercontent.com/bottlepy/bottle/mast
 subprocess.call(["wget","https://files.pythonhosted.org/packages/ca/63/3384ebb3b51af9610086b23ea976e6d27d6d97bf140a76a365bd77a3eb32/mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
 subprocess.call(["tar", "-xzf", "mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
 subprocess.call(["python", "setup.py", "install"], cwd=str(pi_calc_dir+"mpmath-1.1.0"))
-subprocess.call(["rm", "mpmath-1.1.0.tar.gz"], cwd=strpi_calc_dir)
+subprocess.call(["rm", "mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
