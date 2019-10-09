f37e572a5bd8618c708f19145a664e0fa4751b71
krebsalad <johanmgina7@gmail.com>
2019-10-09 22:16:17 +0200

    Update install_picalc_server.py

--- install_picalc_server.py
+++ install_picalc_server.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-current_dir = os.getcwd()	
+current_dir = os.getcwd()
 pi_calc_dir = current_dir + "/"
 subprocess.call(["wget", "https://raw.githubusercontent.com/bottlepy/bottle/master/bottle.py"], cwd=pi_calc_dir)
 subprocess.call(["wget","https://files.pythonhosted.org/packages/ca/63/3384ebb3b51af9610086b23ea976e6d27d6d97bf140a76a365bd77a3eb32/mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
