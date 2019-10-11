8af2b882c413b21f68ebe4040430dc27ed0366c7
krebsalad <johanmgina7@gmail.com>
2019-10-12 00:39:37 +0200

    Update and rename install_picalc_server.py to install_picalc.py

---
+++ install_picalc.py
@@ -0,0 +1,32 @@
+# picalc server installer
+
+import os
+import subprocess
+import sys
+
+# vals
+current_dir = os.getcwd()
+pi_calc_dir = current_dir + "/"
+
+# install loadbalancer
+if(len(sys.argv) > 1):
+   arg = sys.argv[1]
+   if(arg == "lb"):
+        subprocess.call(["wget", "https://files.pythonhosted.org/packages/cc/88/b4df1a3416ea09dd17bea4214d802d87f83afeafe9646ef42712c8e652f6/PumpkinLB-2.0.0.tar.gz"], cwd=pi_calc_dir)
+        subprocess.call(["tar", "-xzf", "PumpkinLB-2.0.0.tar.gz"], cwd=pi_calc_dir)
+        subprocess.call(["rm", "PumpkinLB-2.0.0.tar.gz"], cwd=pi_calc_dir)
+        config_file = open("lb_config.cfg", "w+")
+        config_file.write("[options]\nbuffer_size=4096\n\n[mappings]\n80=192.168.190.100:8080,192.168.190.101:8080")
+        config_file.close()
+        print("downloaded python Pumpkin LB tcp loadbalancing tool and created example config")
+        sys.exit()
+
+# or else install the server
+subprocess.call(["wget", "https://raw.githubusercontent.com/bottlepy/bottle/master/bottle.py"], cwd=pi_calc_dir)
+subprocess.call(["wget","https://files.pythonhosted.org/packages/ca/63/3384ebb3b51af9610086b23ea976e6d27d6d97bf140a76a365bd77a3eb32/mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
+subprocess.call(["tar", "-xzf", "mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
+subprocess.call(["python", "setup.py", "install"], cwd=str(pi_calc_dir+"mpmath-1.1.0"))
+subprocess.call(["rm", "mpmath-1.1.0.tar.gz"], cwd=pi_calc_dir)
+print("dowloaded python mpmath lib, bottle.py")
+sys.exit()
+
