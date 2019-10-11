dbbddf7101e5cbc4d964952e015f73185292c974
krebsalad <johanmgina7@gmail.com>
2019-10-12 00:40:35 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,62 @@
+# Picalc server
+import os
+import sys
+import re
+import socket
+import subprocess
+
+# vars
+current_dir = os.getcwd()
+hostname = socket.gethostname()
+ip_addr = socket.gethostbyname(hostname)
+mode = "server"
+
+# read args
+for arg in sys.argv:
+    if(re.match("ip=",arg)):
+        ip = re.sub("ip=", "", arg)
+        if(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$",ip)):
+            ip_addr = ip
+
+    if(re.match("mode=",arg)):
+        txt = re.sub("mode=", "", arg)
+        if(txt == "lb"):
+            mode = "lb"
+
+
+
+## Loadbalancer ##
+if(mode == "lb"):
+    subprocess.call(["PumpkinLB-2.0.0/PumpkinLB.py", "lb_config.cfg"], cwd=current_dir)
+    sys.exit()
+
+if(mode != "server"):
+    print("run with arguments mode=server or mode=lb")
+    sys.exit()
+
+
+## Picalc server ##
 from bottle import route, run, template
 from mpmath import mp
 
 @route('/PiCalc/<p>')
 def index(p):
+    # read
     precision = int(p)
+
+    # handle non ints
     if(str(type(precision)) != "<type 'int'>"):
         print(str(type(precision)))
         return str("error, precision was not an int")
+
+    # set precision val and calculate pi
     mp.dps = precision
     pi = mp.pi
-    return str("PI: " + str(pi) + "\n")
 
-# run
-run(host='0.0.0.0', port=8080)
+    # return txt
+    return str("Server ip: "+ip_addr+ "\nPI = " + str(pi) + "\n")
+
+
+# run picalc on port 8080
+run(host=ip_addr, port=8080)
+
