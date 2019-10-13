e885221a63be368b305353991a918509f7f3d429
krebsalad <johanmgina7@gmail.com>
2019-10-13 17:46:06 +0200

    Update run.py

--- run.py
+++ run.py
@@ -48,27 +48,9 @@ if(mode == "lb"):
 
 
 ## Picalc server ##
-from bottle import route, run, template
-from mpmath import mp
+if(mode != "server"):
+    print("no such mode " + mode)
+    sys.exit()
+    
 
-@route('/PiCalc/<p>')
-def index(p):
-    # read
-    precision = int(p)
-
-    # handle non ints
-    if(str(type(precision)) != "<type 'int'>"):
-        print(str(type(precision)))
-        return str("error, precision was not an int")
-
-    # set precision val and calculate pi
-    mp.dps = precision
-    pi = mp.pi
-
-    # return txt
-    return str("Server ip: "+ip_addr+ "\nPI = " + str(pi) + "\n")
-
-
-# run picalc on port 8080
-run(host=ip_addr, port=8080)
 
