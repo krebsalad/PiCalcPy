dfffc60380d7e7a976de6dbe3698128224f68b00
krebsalad <johanmgina7@gmail.com>
2019-10-14 18:32:57 +0200

    Update picalc_server.py

--- picalc_server.py
+++ picalc_server.py
@@ -1,12 +1,16 @@
 from bottle import route, run, template
 from mpmath import mp
 import socket
+import time
 
 hostname = socket.gethostname()
 ip_addr = socket.gethostbyname(hostname)
 
 @route('/PiCalc/<p>')
 def index(p):
+    # set time
+    start_time = time.time()
+    
     # read
     precision = int(p)
 
@@ -19,8 +23,11 @@ def index(p):
     mp.dps = precision
     pi = mp.pi
 
+    # end time
+    processing_time = time.time() - start_time
+    
     # return txt
-    return str("Server ip: "+ip_addr+ "\nPI = " + str(pi) + "\n")
+    return str("Server ip: "+ip_addr+ "\nTime took: "+ str(processing_time) + " seconds\nPI = " + str(pi) + "\n")
 
 # run picalc on port 8080
 def run_server(port_num=8080):
